5f2b17b1ee221e577118331cafc53313196c9551
Alex87464 <alexoliva87464@gmail.com>
2023-09-29 01:56:39 -0300

    Ejercicio 2 completado

--- semana-3/ejercicio2.py
+++ semana-3/ejercicio2.py
@@ -4,8 +4,10 @@
 # Ejemplo si el usuario ingresa 5, el programa debe imprimir el resultado 
 # de 1 + 2 + 3 + 4 + 5 = 15
 
+# Creamos la variable donde guardamos el número ingresado por el usuario
 numero = int(input("Ingrese un número: "))
-suma = 0
-for i in range(1, numero + 1):
-    suma = suma + i
-print(suma)
+
+suma = 0 # Creamos la variable donde guardamos la suma de los números
+for i in range(1, numero + 1): # Iteramos desde 1 hasta el número ingresado
+    suma = suma + i # Sumamos el valor de i a la variable suma
+print(suma) # Imprimimos el resultado de la suma
