f28d5d27241c788c329e5d6201eda0b49f9a2aed
Alex87464 <alexoliva87464@gmail.com>
2023-10-04 07:02:09 -0300

    Ejercicio 14 completado

---
+++ semana-4/ejercicio14.py
@@ -0,0 +1,12 @@
+"""
+14-Crea una función llamada encontrar_maximo que tome una lista de números
+como parámetro y devuelva el número máximo de la lista.
+"""
+
+def encontrar_maximo(lista_numeros):
+    return print(max(lista_numeros)) # La funcion max retorna el valor máximo de un iterable    
+    
+encontrar_maximo([20, 14, 3, 9, 44, 32, 5]) # Debería retornar 44 dado que es el número mas grande de la lista
+
+# Recurso sobre max():
+# https://interactivechaos.com/es/python/function/max
