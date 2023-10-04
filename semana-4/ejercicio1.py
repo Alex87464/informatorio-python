2c8539c9042d1680aafe3417c874ec7c3cc7b5e2
Alex87464 <alexoliva87464@gmail.com>
2023-10-04 05:24:27 -0300

    Ejercicio 1 completado

---
+++ semana-4/ejercicio1.py
@@ -0,0 +1,10 @@
+"""
+1-Crea una función llamada suma que tome dos números como parámetros y
+devuelva la suma de ellos.
+"""
+
+def suma(a,b):
+    print(a+b)
+    
+    
+suma(2,3) # Deberiía imprimir 5
