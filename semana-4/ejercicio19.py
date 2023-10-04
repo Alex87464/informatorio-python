1bf2584bb538cb7b3fddb174e2a9cfd0b7a7433d
Alex87464 <alexoliva87464@gmail.com>
2023-10-04 08:22:18 -0300

    Comentarios agregados

--- semana-4/ejercicio19.py
+++ semana-4/ejercicio19.py
@@ -5,6 +5,12 @@ es bisiesto si es divisible por 4, pero no por 100, a menos que también sea
 divisible por 400.
 """
 
+# * Fórmula del año bisiesto: [p y ¬q] ó [r]
+# p: Es divisible entre 4
+# ¬q: No es divisible entre 100
+# r: Es divisible entre 400
+# ? Fórmula alternativa mas eficiente: p y [¬q ó r]
+
 def es_bisiesto(year):
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 :
         print(f"El año {year} es bisiesto")
