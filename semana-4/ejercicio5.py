b2547987eeb022eea1f566e9e3bf46aa4b46ef6a
Alex87464 <alexoliva87464@gmail.com>
2023-10-04 05:41:38 -0300

    Ejercicio 5 corregido

--- semana-4/ejercicio5.py
+++ semana-4/ejercicio5.py
@@ -4,10 +4,11 @@ parámetros y devuelva Es divisible si el primer número es divisible por el
 segundo número, o No es divisible en caso contrario.
 """
 
+
 def es_divisible(numero1, numero2):
-    if(numero1 / numero2 == 0):
+    if(numero1 % numero2 == 0): 
         return print(f"{numero1} es divisible por {numero2}")
     else:
         return print(f"{numero1} no es divisible por {numero2}")
     
-es_divisible(10, 2) # Debería devolver True
+es_divisible(10, 2) # Debería devolver True dado que 10 % 2 = 0
